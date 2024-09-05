28fc409e705e77a74f11e4d909528e36c50148e5
Julia Souza <julia.souza@ciag.org.br>
2024-09-05 14:45:18 -0300

    add vuex

--- alura-tracker/src/components/BarraLateral.vue
+++ alura-tracker/src/components/BarraLateral.vue
@@ -11,20 +11,18 @@
                 <li>
                     <RouterLink to="/" class="link">
                         <i class="fas fa-tasks"></i>
-                        tarefas
+                        Tarefas
                     </RouterLink>
                 </li>
                 <li>
                     <RouterLink to="/listaProjetos" class="link">
                         <i class="fas fa-project-diagram"></i>
-                        projetos
+                        Projetos
                     </RouterLink>
                 </li>
             </ul>
-
         </nav>
     </header>
-
 </template>
 
 <script lang="ts">
@@ -37,55 +35,50 @@ export default defineComponent({
         return {
             modoEscuroAtivo: false
         }
-    },
-    computed: {
-        textoBotao(){
-            if(this.modoEscuroAtivo){
-                return 'Desativar modo escuro'
-            }
-            return 'Ativar modo escuro'
-        }
-    },
+    },    
     methods: {
-        alterarTema(){
+        alterarTema(): void{
             this.modoEscuroAtivo = !this.modoEscuroAtivo
             this.$emit('aoTemaAlterado', this.modoEscuroAtivo)
-
         }
-    }
+    },
+    computed: {
+        textoBotao(): string{
+            return this.modoEscuroAtivo ? 'Claro' : 'Escuro'
+        }
+    },
 })
 </script>
 
 <style scoped>
+.h1 {
+    text-align: center;
+}
+.strong {
+    color: #f95738;
+}
 header {
-    padding: 1rem;
     background: #0d3b66;
     width: 100%;
     height: 100vh;
-    text-align: center;
+    padding: 2rem;
 }
 
 @media only screen and (max-width: 768px) {
     header {
-        padding: 2.5rem;
-        height: auto;
-    }
-}
-
-.button{
-    background-color: var(--bg-primario);
-    color: var(--texto-primario);
+    height: auto;
+  }
 }
 .panel li {
-    color: #fff;
+  margin: 8px 0;
 }
-.link{
-    color: #fff;
+.link {
+  color: #fff;
 }
 .link:hover {
-    color:#FAF0CA;
+  color: #FAF0CA;
 }
-.link.router-link-active{
-    color:#FAF0CA;    
+.link.router-link-active {
+  color: #FAF0CA;
 }
 </style>
--- alura-tracker/src/views/ListaProjetos.vue
+++ alura-tracker/src/views/ListaProjetos.vue
@@ -1,11 +1,68 @@
 <template>
-    <h1>Projetos</h1>
+    <section class="projetos">
+        <h1 class="title">Projetos</h1>
+        <form @submit.prevent="salvar">
+            <div class="field">
+                <label for="nomeDoProjeto" class="label">Nome do Projeto</label>
+                <input
+                type="text"
+                class="input"
+                v-model="nomeDoProjeto"
+                id="nomeDoProjet"/>
+            </div>
+            <div class="field">
+                <button class="button" type="submit">
+                    Salvar
+                </button>
+            </div>
+        </form>
+        <table class="table is-fullwidth">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nome</th>
+                </tr>
+            </thead>
+            <tbody v-for="projeto in projetos" :key="projeto.id">
+                <tr>
+                    <td>{{projeto.id}}</td>
+                    <td>{{projeto.nome}}</td>
+                </tr>
+            </tbody>
+
+        </table>
+    </section>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { computed, defineComponent } from 'vue';
+import { useStore } from '@/store';
 
 export default defineComponent({
-    name: 'ListaProjetos'
+    name: 'ListaProjetos',
+    data() {
+        return {
+            nomeDoProjeto: ''
+        };
+    },
+    methods: {
+        salvar(){
+            this.store.commit('ADICIONA_PROJETO', this.nomeDoProjeto)
+            this.nomeDoProjeto = '';
+        }
+    },
+    setup () {
+        const store = useStore()
+        return {
+            store,
+            projetos: computed(() => store.state.projetos)
+        }
+    }
 })
 </script>
+
+<style scoped>  
+.projetos {
+    padding: 1.25rem;
+}
+</style>
